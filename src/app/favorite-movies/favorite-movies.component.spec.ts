a45db768ddaf64e59ec4f3657e317376f31541fc
MalikWaseemJaved <engr.wasim2010@gmail.com>
2021-10-07 11:03:12 +0500

    Jest configured, first test written and ran pass

--- src/app/favorite-movies/favorite-movies.component.spec.ts
+++ src/app/favorite-movies/favorite-movies.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { FavoriteMoviesComponent } from './favorite-movies.component';
 
@@ -21,5 +22,11 @@ describe('FavoriteMoviesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('should have a title', () => {
+    const titleElements = fixture.debugElement.queryAll(By.css('h1'));
+    expect(titleElements.length).toBe(1);
+    expect(titleElements[0].nativeElement.innerHTML).toBe('Favorite movies');
   });
 });
